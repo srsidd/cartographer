934e99341d397b16382a129fec83d5b5a7c00f51
Siddharth Srivatsa <ssrivatsa@houstonmechatronics.com>
2018-01-19 02:09:24 -0600

    Add comments

--- cartographer/client_iface.py
+++ cartographer/client_iface.py
@@ -10,12 +10,17 @@ log_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 logger.addHandler(log_handler)
 
 class CartographerIface(object):
+""" Class to interface with the Cartographer client """
 
     def __init__(self, cartographer_client):
         self.client = cartographer_client
         self.addr = None
 
     def use_custom_geoCoding(self):
+        """ Method to send a http GET req to a custom url 
+            
+            Returns: (String) Response sent by the server or None
+        """
         query_url = raw_input("Please enter the entire url to be querried:\n")
         response = self.client.request(url=query_url)
         if api_response is None:
@@ -27,20 +32,32 @@ class CartographerIface(object):
         return response
 
     def use_google_geoCoding(self):
+        """ Method to send a http GET req to the google geo coding req
+
+            Returns: (GeoPoint) Address, latitude and longitude of the address
+        """
         if self.addr is None:
             self.addr = self.client.get_address()
         g_data = self.client.google_geocode_service(address=self.addr)
-        # print g_data
         return g_data
 
     def use_here_geoCoding(self):
+        """ Method to send a http GET req to the here geo coding req
+
+            Returns: (GeoPoint) Address, latitude and longitude of the address
+        """
         if self.addr is None:
             self.addr = self.client.get_address()
         g_data = self.client.here_geocode_service(address=self.addr)
-        # print g_data
         return g_data
 
     def get_json_config(self):
+        """ Method to get the config path to retrieve the latitude, longitude and the address
+            from the response returned by the google and here API's
+
+            Returns: (dict) Dictionary for each API with config paths for Address, latitude
+            and longitude of the address
+        """
         google_json_config = [self.client.get_google_lat_long_config(), self.get_google_addr_config(), self.get_google_status_config()]
         here_json_config = [self.client.get_here_lat_long_config(), self.client.get_here_addr_config(), self.get_here_status_config()]
         
@@ -48,7 +65,11 @@ class CartographerIface(object):
                           'here_json_config': here_json_config}
         return default_config
 
-    def get_default_creds(self):
+    def get_api_creds(self):
+        """ Method to retrieve the credentials to access the google and here API's
+
+            Returns: (dict) Dictionary for each API with their corresponding api keys and ids
+        """
         google_api_cred = self.client.get_google_api_creds()
         here_api_cred = self.client.get_here_api_creds()
         default_creds = {'google_api_cred': google_api_cred, 
@@ -56,6 +77,14 @@ class CartographerIface(object):
         return default_creds
 
     def set_creds(self, google_api_cred=None, here_creds=None):
+        """ Method to set the credentials to access the google and here API's
+
+            google_api_cred: (string) The api google api key which can be got from
+                            https://developers.google.com/maps/documentation/geocoding/get-api-key
+
+            here_creds: (list) [app_id, app_code] here API parameters which can be got from
+                        https://developer.here.com/documentation/geocoder/common/credentials.html 
+        """
         if google_api_cred is not None:
             self.client.set_google_api_creds(google_api_cred)
         if isinstance(here_creds, list) and None not in here_creds:
